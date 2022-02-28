14e8e80a155044219d43db7c8393862a8a5f1025
Pascal Klaßen <p.klassen221199@gmail.com>
2022-02-28 20:52:26 +0100

    Mix Textures.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 #include <iostream>
 #include <string>
+
+#include "Debug.h"
 
 void CheckShaderInfoLog(GLuint shader)
 {
@@ -32,7 +35,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    window = glfwCreateWindow(960, 540, "OpenGL Lab", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "OpenGL Lab", NULL, NULL);
 
     if (!window)
     {
@@ -53,11 +56,11 @@ int main()
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     GLfloat vertices[] =
-    { //  x      y    //  R     G     B
-        -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,
-         0.5f,  0.5f,    0.0f, 1.0f, 0.0f,
-         0.5f, -0.5f,    0.0f, 0.0f, 1.0f,
-        -0.5f, -0.5f,    1.0f, 1.0f, 1.0f,
+    { //  x      y    //  R     G     B    //  S     T
+        -0.5f,  0.5f,    1.0f, 0.0f, 0.0f,    0.0f, 1.0f, // Top-left
+         0.5f,  0.5f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, // Top-right
+         0.5f, -0.5f,    0.0f, 0.0f, 1.0f,    1.0f, 0.0f, // Bottom-right
+        -0.5f, -0.5f,    1.0f, 1.0f, 1.0f,    0.0f, 0.0f, // Bottom-left
     };
 
     GLuint indices[] =
@@ -85,11 +88,14 @@ int main()
         
         in vec2 i_Position;
         in vec3 i_Color;
+        in vec2 i_TexCoord;
 
         out vec3 p_Color;
+        out vec2 p_TexCoord;
     
         void main()
         {
+            p_TexCoord = i_TexCoord;
             p_Color = i_Color;
             gl_Position = vec4(i_Position, 0.0, 1.0);
         }
@@ -103,12 +109,18 @@ int main()
         #version 330 core
 
         in vec3 p_Color;
+        in vec2 p_TexCoord;
 
         out vec4 color;
+
+        uniform sampler2D texKitten;
+        uniform sampler2D texPuppy;
         
         void main()
         {
-            color = vec4(p_Color, 1.0);
+            vec4 colKitten = texture(texKitten, p_TexCoord);
+            vec4 colPuppy = texture(texPuppy, p_TexCoord);
+            color = mix(colKitten, colPuppy, 0.5);
         }
     )glsl";
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -124,11 +136,49 @@ int main()
 
     GLint posAttrib = glGetAttribLocation(shaderProgram, "i_Position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
     
     GLint colAttrib = glGetAttribLocation(shaderProgram, "i_Color");
     glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*) (2 * sizeof(GLfloat)));
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*) (2 * sizeof(GLfloat)));
+
+    GLint texAttrib = glGetAttribLocation(shaderProgram, "i_TexCoord");
+    glEnableVertexAttribArray(texAttrib);
+    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*) (5 * sizeof(GLfloat)));
+
+    GLint width, height;
+    stbi_set_flip_vertically_on_load(1);
+    GLubyte* img;
+    GLuint textures[2];
+    GL_CHECK(glGenTextures(2, textures));
+
+    img = stbi_load("sample.png", &width, &height, 0, 4);
+    GL_CHECK(glActiveTexture(GL_TEXTURE0));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, textures[0]));
+    GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img));
+    stbi_image_free(img);
+    GL_CHECK(GLint texKitten = glGetUniformLocation(shaderProgram, "texKitten"));
+    GL_CHECK(glUniform1i(texKitten, 0));
+    /* Wrapping */
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+    /* Filtering */
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+
+    img = stbi_load("sample2.png", &width, &height, 0, 4);
+    GL_CHECK(glActiveTexture(GL_TEXTURE1));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, textures[1]));
+    GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img));
+    stbi_image_free(img);
+    GL_CHECK(GLint texPuppy = glGetUniformLocation(shaderProgram, "texPuppy"));
+    GL_CHECK(glUniform1i(texPuppy, 1));
+    /* Wrapping */
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+    /* Filtering */
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 
     while (!glfwWindowShouldClose(window))
     {
@@ -143,6 +193,8 @@ int main()
 
         glfwPollEvents();
     }
+
+    glDeleteTextures(2, textures);
 
     glDeleteProgram(shaderProgram);
     glDeleteShader(fragmentShader);
